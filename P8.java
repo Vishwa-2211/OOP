44547abaef52c2878a6ac8c321202e7380e95677
Vishwa-2211 <137034947+Vishwa-2211@users.noreply.github.com>
2026-01-20 09:53:12 +0530

    Add files via upload

---
+++ P8.java
@@ -0,0 +1,11 @@
+class P8{
+    public static void main(String[] args) {
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        int sum = a + b;
+	System.out.println("250393107014");
+        System.out.println("Sum = " + sum);
+    }
+}
